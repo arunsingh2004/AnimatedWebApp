6da2f3183a392ba57c5343de159a6463666ec35d
Arun Singh <aruns.ug21.cs@nitp.ac.in>
2023-08-30 22:51:36 +0530

    add footer and text

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
-import { socials } from '../constants';
+import { motion } from "framer-motion";
+import { socials } from "../constants";
 
-import styles from '../styles';
-import { footerVariants } from '../utils/motion';
+import styles from "../styles";
+import { footerVariants } from "../utils/motion";
 
 const Footer = () => (
   <motion.footer
@@ -16,30 +16,29 @@ const Footer = () => (
     <div className="footer-gradient" />
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
-        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-          Enter the Metaverse
-        </h4>
-        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+        <h5 className="font-bold md:text-[64px] text-[44px] text-black">
+          Ahead App
+        </h5>
+        <button
+          type="button"
+          className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]"
+        >
           <img
             src="/headset.svg"
             alt="headset"
             className="w-[24px] h-[24px] object-contain"
           />
-          <span className="font-normal text-[16px] text-white">
-            Enter Metaverse
-          </span>
+          <span className="font-normal text-[16px] text-black">Arun's App</span>
         </button>
       </div>
 
       <div className="flex flex-col">
-        <div className="mb-[50px] h-[2px] bg-white opacity-10" />
+        <div className="mb-[50px] h-[2px] bg-black opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">
-            METAVERUS
-          </h4>
-          <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+          <h4 className="font-extrabold text-[24px] text-black">Ahead</h4>
+          <p className="font-normal text-[14px]  opacity-50 text-black">
+            Copyright © 2023. All rights reserved.
           </p>
 
           <div className="flex gap-4">
@@ -48,7 +47,7 @@ const Footer = () => (
                 key={social.name}
                 src={social.url}
                 alt={social.name}
-                className="w-[24px] h-[24px] object-contain cursor-pointer"
+                className="w-[24px] h-[24px] object-contain cursor-pointer bg-black rounded-full "
               />
             ))}
           </div>
